cbbdd043737356ea56c73bd57df4f0d84021fc22
Man Hoang <manminhhoang@gmail.com>
2017-11-06 22:06:51 -0500

    Relax regex

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const urlRegex = require('url-regex');
 const normalizeUrl = require('normalize-url');
 
 module.exports = (str, opts) => {
-	const urls = str.match(urlRegex());
+	const urls = str.match(urlRegex({strict: false}));
 	const ret = urls ? urls.map(url => normalizeUrl(url.trim().replace(/\.+$/, ''), opts)) : [];
 	return new Set(ret);
 };
